16d4ad2f4b2d239e39bc2e425c69aedd76429c9d
primadita <pulomas10@hotmail.com>
2025-07-02 22:21:47 +0200

    feat: render and add note function

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let notes = [];
 // ich muss definieren wo sie anzuzeigen sind
 function renderNotes(){
 
-    let contentRef = document.getElementById('content');
+    let contentRef = document.getElementById('display-all');
+    contentRef.innerHTML = "";
     
     for (let indexNote = 0; indexNote < notes.length; indexNote++){
         const singleNote = notes[indexNote];
@@ -17,8 +18,25 @@ function renderNotes(){
 
 
 // notizen hinzufügen
-function addNotes(){
+function addSingleNote(){
+    let noteInputRef = document.getElementById("input-note");
+    let noteInput = noteInputRef.value ;
+    notes.push(noteInput);
 
+    renderNotes();
+
+    noteInputRef.value = "";
 }
+
+
 // notizen löschen
+
+// welche Notiz muss gelöscht werden
+// wann muss die Notiz gelöscht werden
+// Anzeige updaten
+
+// function deleteSingleNote(indexNote){
+//     notes.splice(indexNote,1);
+//     renderNotes();
+// }
 // notizen archivieren
