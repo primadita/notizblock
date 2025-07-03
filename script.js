a84f2fd0f3b2d1a57e1f7dd58e9a9ae8ddcce86d
primadita <pulomas10@hotmail.com>
2025-07-03 13:36:09 +0200

    feat: all functions work (add, delete, archiv) except local storage

--- script.js
+++ script.js
@@ -1,56 +1,114 @@
+let notes = [
+    {
+        title: "a",
+        desc: "shopping"
+    },
+    {
+        title: "b",
+        desc: "calling"
+    }
+];
+let archives = [
+    {
+        title: "c",
+        desc: "washing"
+    },
+    {
+        title: "d",
+        desc: "cleaning"
+    }
+];
+let trashbin =[
+    {
+        title: "e",
+        desc: "TV"
+    },
+    {
+        title: "f",
+        desc: "watering plants"
+    }
+];
 
-let notes = ["banana", "orange"];
-let trashbin =[];
 
 
 // ich muss definieren wo sie anzuzeigen sind
-function renderNotes(){
-
-    let contentRef = document.getElementById('display-all');
-    contentRef.innerHTML = "";
-    
-    for (let indexNote = 0; indexNote < notes.length; indexNote++){
-        const singleNote = notes[indexNote];
-        contentRef.innerHTML += getNotesTemplate(indexNote);
-    }
-    
-    
-}
-
-
-
-// notizen hinzufügen
-function addSingleNote(){
-    let noteInputRef = document.getElementById("input-note");
-    let noteInput = noteInputRef.value ;
-    notes.push(noteInput);
-    console.log(notes);
+function init(){
     renderNotes();
-
-    noteInputRef.value = "";
-}
-
-// Löschen function
-function deleteSingleNote(indexNote){
-    let trashNote = notes.splice(indexNote,1);
-    trashbin.push(trashNote);
-    renderNotes();
+    renderArchives();
     renderTrashNotes();
 }
 
+function renderNotes(){
+    const contentRef = document.getElementById('all-notes');
+    contentRef.innerHTML = "";
+    
+    for (let indexNote = 0; indexNote < notes.length; indexNote++){
+        // const singleNote = notes[indexNote];
+        contentRef.innerHTML += getNotesTemplate(indexNote);
+    } 
+}
+
+function renderArchives(){
+    const archiveRef = document.getElementById('archive-notes');
+    archiveRef.innerHTML = "";
+    
+    for (let indexArchive = 0; indexArchive < archives.length; indexArchive++){
+        // const singleArchive = archives[indexArchive];
+        archiveRef.innerHTML += getArchiveTemplate(indexArchive);
+    } 
+}
 function renderTrashNotes(){
     let trashbinRef = document.getElementById('trash');
     trashbinRef.innerHTML ="";
 
     for (let indexTrashbin = 0; indexTrashbin < trashbin.length; indexTrashbin++){
-        let singleTrash = trashbin[indexTrashbin];
+        // let singleTrash = trashbin[indexTrashbin];
         trashbinRef.innerHTML += getTrashbinTemplate(indexTrashbin);
     }
 }
 
-function deleteNoteForever(indexTrashbin){
+function addSingleNote(){
+    const noteTitleInputRef = document.getElementById("input-title");
+    const noteInputRef = document.getElementById("input-desc"); 
+    
+    let noteInput = {};
+    noteInput.title = noteTitleInputRef.value;
+    noteInput.desc = noteInputRef.value;
+    
+    notes.push(noteInput);
+    renderNotes();
+
+    noteTitleInputRef.value = "";
+    noteInputRef.value = "";
+}
+
+
+function deleteNote(originarray, index){
+    let gotoTrash = originarray;
+    gotoTrash = gotoTrash.splice(index,1);
+    trashbin.push(gotoTrash[0]);
+    
+    init();
+}
+
+function sendToArchive(indexNote){
+    let gotoArchive = notes.splice(indexNote,1);
+    archives.push(gotoArchive[0]);
+    
+    renderNotes();
+    renderArchives();
+}
+
+function sendToNote(originarray, index){
+    let gotoNote = originarray;
+    gotoNote = gotoNote.splice(index,1);
+    notes.push(gotoNote[0]);
+    
+    init();
+}
+
+
+function deleteIrrevocably(indexTrashbin){
     trashbin.splice(indexTrashbin,1);
     renderTrashNotes();
 }
-//
-// notizen archivieren
--- template.js
+++ template.js
@@ -1,11 +1,32 @@
 function getNotesTemplate(indexNote){
     return `
-            <p>${notes[indexNote]}</p><button onclick="deleteSingleNote(${indexNote})">Delete</button>
-        `
+        <div class="postit">
+            <h3>${notes[indexNote].title}<h3>
+            <p>${notes[indexNote].desc}</p>
+            <button class="delete-btn" onclick="deleteNote(notes, ${indexNote})">X</button>
+            <button class="archive-btn" onclick="sendToArchive(${indexNote})">Archive</button>
+        </div>
+    `
+}
+
+function getArchiveTemplate(indexArchive){
+    return `
+        <div class="postit">
+            <h3>${archives[indexArchive].title}<h3>
+            <p>${archives[indexArchive].desc}</p>
+            <button class="delete-btn" onclick="deleteNote(archives, ${indexArchive})">X</button>
+            <button class="main-btn" onclick="sendToNote(archives, ${indexArchive})">Back</button>
+        </div>
+    `
 }
 
 function getTrashbinTemplate(indexTrashbin){
     return `
-            <p>${trashbin[indexTrashbin]}</p><button onclick="deleteNoteForever(${indexTrashbin})">Delete forever</button>
-        `
+        <div class="postit">
+            <h3>${trashbin[indexTrashbin].title}<h3>
+            <p>${trashbin[indexTrashbin].desc}</p>
+            <button class="delete-btn" onclick="deleteIrrevocably(${indexTrashbin})">DEL</button>
+            <button class="main-btn" onclick="sendToNote(trashbin, ${indexTrashbin})">Back</button>
+        </div>
+    `
 }
